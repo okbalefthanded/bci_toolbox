7701c5b275adbc9211a9be6a4554164c797d7c6e
okba bekhelifi <okba.bekhelifi@gmail.com>
2018-07-31 02:55:02 +0200

    - Working on issue #2 	- Migrated FBCCA  On branch dev 	modified:   define_approach_SSVEP.m 	modified:   machine_learning/classification/ml_trainFBCCA.m

--- define_approach_SSVEP.m
+++ define_approach_SSVEP.m
@@ -2,21 +2,22 @@
 % 03-21-2018
 % Okba Bekhelifi, <okba.bekhelif@univ-usto.dz>
 tic
-% set.title = 'SSVEP_EXOSKELETON';
+set.title = 'SSVEP_EXOSKELETON';
 % set = 'SSVEP_DEMO'; 
 % set.title = 'SSVEP_TSINGHUA';
-set.title = 'SSVEP_SANDIEGO';
+% set.title = 'SSVEP_SANDIEGO';
 set.mode = 'SM';
-report = 1;
+report = 0;
 %% vanilla CCA
-approach.classifier.learner = 'CCA';
-approach.classifier.options.harmonics = 2;
+% approach.classifier.learner = 'CCA';
+% approach.classifier.options.harmonics = 2;
 %% L1 Multiway CCA
 % approach.classifier.learner = 'L1MCCA';
 % approach.classifier.options.harmonics = 2;
 % approach.classifier.options.max_iter = 200; % the maximal number of iteration for running L1MCCA
 % approach.classifier.options.n_comp = 1;  % number of projection components for learning the reference signals
-% approach.classifier.options.lambda = 0.02; % regularization parameter for the 3rd-way (i.e., trial-way), which can be more precisely decided by cross-validation
+% % approach.classifier.options.lambda = 0.02; % regularization parameter for the 3rd-way (i.e., trial-way), which can be more precisely decided by cross-validation
+% approach.classifier.options.lambda = [0, 1];
 %% Mset CCA
 % approach.classifier.learner = 'MSETCCA';
 % approach.classifier.options.n_comp = 1;
@@ -31,14 +32,19 @@ approach.classifier.options.harmonics = 2;
 % approach.classifier.options.num_fbs = 5;
 % approach.classifier.options.is_ensemble = 1;
 %% FBCCA
-% approach.classifier.learner = 'FBCCA';
-% approach.classifier.options.harmonics = 5;
-% approach.classifier.options.nrFbs = 5;
+approach.classifier.learner = 'FBCCA';
+approach.classifier.options.harmonics = 5;
+approach.classifier.options.nrFbs = 5;
+approach.classifier.options.a = [0, 2.5];
+approach.classifier.options.b = [0, 1.5];
 %% ITCCA
 % approach.classifier.learner = 'ITCCA';
 %%
 approach.cv.method = 'KFOLD';
-approach.cv.nfolds = 0;
+% approach.cv.nfolds = 0;
+approach.cv.nfolds = 5;
+approach.cv.parallel.isWorker = 1;
+approach.cv.parallel.nWorkers = 3;
 %%
 [results, output, model] = run_analysis_SSVEP(set, approach, report);
 t = toc;
--- machine_learning/classification/ml_trainFBCCA.m
+++ machine_learning/classification/ml_trainFBCCA.m
@@ -4,7 +4,9 @@ function [model] = ml_trainFBCCA(features, alg, cv)
 % created 07-01-2018
 % last modified -- -- --
 % Okba Bekhelifi, <okba.bekhelif@univ-usto.dz>
-
+if(isfield(alg,'o') || isfield(alg, 'n') || isfield(cv, 'n'))
+    [alg, cv] = recoverStructs(alg, cv);
+end
 if(~isfield(alg, 'options'))
     alg.options.harmonics = 5;
     alg.options.nrFbs = 5;
@@ -24,27 +26,120 @@ end
 stimuli_count = length(frqs);
 reference_signals = cell(1, stimuli_count);
 
-
-if(cv.nfolds == 0)   
-  
+if(cv.nfolds == 0)
     if(~isfield(alg.options, 'fbCoefs'))
-        model.fbCoefs = (1:alg.options.nrFbs).^(-1.25)+0.25;
-    end    
+        model.fbCoefs = (1:alg.options.nrFbs).^(-alg.options.a)+alg.options.b;
+    end
     % construct reference signals
     for stimulus=1:stimuli_count
         reference_signals{stimulus} = refsig(frqs(stimulus),...
-                                             features.fs, ...
-                                             samples, ...
-                                             alg.options.harmonics...
-                                             );
+            features.fs, ...
+            samples, ...
+            alg.options.harmonics...
+            );
     end
+    model.alg.learner = 'FBCCA';
+    model.ref = reference_signals;
+    model.nrFbs = alg.options.nrFbs;
 else
-    % TODO
-    % model.fbCoefs = ; cross-val determination
+       % parallel settings
+    settings.isWorker = cv.parallel.isWorker;
+    settings.nWorkers = cv.parallel.nWorkers;
+    datacell.data = features;
+    %     cv split, kfold
+    datacell.fold = ml_crossValidation(cv, size(features.signal, 3));
+    %     Train & Predict functions
+    %     SharedMatrix bug, fieldnames should have same length
+    fHandle.tr = 'ml_trainFBCCA';
+    fHandle.pr = 'ml_applyFBCCA';
+    %     generate param cell
+    paramcell = genParams(alg, settings);
+    %     start parallel CV
+    [res, resKeys] = startMaster(fHandle, datacell, paramcell, settings);
+    %     select_best_hyperparam
+    [best_worker, best_evaluation] = getBestParamIdx(res, paramcell);
+    best_param = paramcell{best_worker}{best_evaluation}{1};
+    %     detach Memory
+    SharedMemory('detach', resKeys, res);
+    %     kill slaves processes
+    terminateSlaves;
+    alg = best_param;
+    cv.nfolds = 0;
+    cv = fRMField(cv, 'parallel');
+    model = ml_trainFBCCA(features, alg, cv);
 end
-model.alg.learner = 'FBCCA';
-model.ref = reference_signals;
-model.nrFbs = alg.options.nrFbs;
-
 end
-
+%%
+function [alg, cv] = recoverStructs(alg, cv)
+if(isfield(alg,'o'))
+    if(isfield(alg.o,'h'))
+        [alg.options.('harmonics')] = alg.o.('h');
+        fields = {'h'};
+    end
+    if(isfield(alg.o,'n'))
+        [alg.options.('nrFbs')] = alg.o.('n');
+        fields = {fields{:}, 'n'};
+    end
+    if(isfield(alg.o,'a'))
+        [alg.options.('a')] = alg.o.('a');
+        fields = {fields{:}, 'a'};
+    end
+     if(isfield(alg.o,'b'))
+        [alg.options.('b')] = alg.o.('b');
+        fields = {fields{:}, 'a'};
+    end
+end
+if(isfield(cv, 'n'))
+    [cv.('nfolds')] = cv.('n');
+end
+alg = fRMField(alg, fields);
+cv = fRMField(cv, 'n');
+end
+%%
+function paramcell = genParams(alg, settings)
+if(size(alg.options.a,2)==2)
+    a = alg.options.a(1):0.25:alg.options.a(2);
+else if(numel(alg.options.a) > 2)
+        a = alg.options.a;
+    else
+        % default range
+        a = 0:0.25:1;
+    end
+end
+if(size(alg.options.b,2)==2)
+    b = alg.options.b(1):0.25:alg.options.b(2);
+else if(numel(alg.options.b) > 2)
+        b = alg.options.b;
+    else
+        % default range
+        b = 0:0.25:1;
+    end
+end
+searchSpace = length(a)* length(b);
+[nWorkers, paramsplit, offset] = getRessources(settings, searchSpace);
+paramcell = cell(1, nWorkers);
+cv.n = 0;
+alg.o.h = alg.options.harmonics;
+alg.o.n = alg.options.nrFbs;
+alg = fRMField(alg, {'options', 'learner'});
+m = 1;
+n = 1;
+off = 0;
+for i=1:nWorkers
+    tmp = cell(1, paramsplit+off);
+    for k=1:(paramsplit+off)
+        alg.o.a = a(m);       
+        alg.o.b = b(n);      
+        tmp{k} = {alg, cv};
+        n = n + 1;
+        if(n > length(b) && m < length(a))
+            n = 1;
+            m = m+1;
+        end
+    end
+    paramcell{i} = tmp;
+    if((nWorkers - i) == offset)
+        off = 1;
+    end
+end
+end
