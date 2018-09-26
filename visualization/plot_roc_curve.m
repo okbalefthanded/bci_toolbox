a05eb5cb4e9f0a873f1e8a7ee99f35cfb5878f82
okba bekhelifi <okba.bekhelifi@gmail.com>
2018-09-26 16:08:28 +0100

    On branch dev - Added Roc curve plot and AUC  Changes to be committed: 	new file:   visualization/plot_roc_curve.m

---
+++ visualization/plot_roc_curve.m
@@ -0,0 +1,19 @@
+function [AUC] = plot_roc_curve(output)
+%PLOT_ROC_CURVE Summary of this function goes here
+%   Detailed explanation goes here
+% date created 09-26-2016
+% last modified -- -- --
+% Okba Bekhelifi, <okba.bekhelif@univ-usto.dz>
+
+pred = output.score;
+y = output.trueClasses';
+classifier = output.alg.learner;
+[X,Y,T,AUC] = perfcurve(y,pred,1);
+n = length(Y);
+random_guess = linspace(0,1,n);
+plot(random_guess, random_guess, 'r--');hold on,
+plot(X,Y)
+xlabel('False positive rate'); ylabel('True positive rate')
+title(['ROC for classification by ',classifier,' AUC = ',num2str(AUC)])
+end
+
