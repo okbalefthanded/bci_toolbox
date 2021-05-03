1e349fe0607d57669659a4b5accda9bb97ded8ca
okba bekhelifi <okba.bekhelifi@gmail.com>
2021-05-04 01:42:59 +0200

    - Preparing a merge with master  On branch dev  Changes to be committed: 	modified:   define_approach_SSVEP.m

--- define_approach_SSVEP.m
+++ define_approach_SSVEP.m
@@ -4,8 +4,8 @@
 tic
 % set.title = 'SSVEP_EXOSKELETON';
 % set.title = 'SSVEP_DEMO'; 
-% set.title = 'SSVEP_TSINGHUA_JFPM';
-set.title = 'SSVEP_SANDIEGO';
+set.title = 'SSVEP_TSINGHUA_JFPM';
+% set.title = 'SSVEP_SANDIEGO';
 % set.title = 'SSVEP_LARESI';
 set.mode = 'SM';
 report = 0;
@@ -14,13 +14,13 @@ report = 0;
 % approach.classifier.options.harmonics = 3;
 % approach.classifier.options.mode = 'sync';
 %% L1 Multiway CCA
-% approach.classifier.learner = 'L1MCCA';
-% approach.classifier.options.harmonics = 2;
-% approach.classifier.options.max_iter = 200; % the maximal number of iteration for running L1MCCA
-% approach.classifier.options.n_comp = 1;  % number of projection components for learning the reference signals
-% approach.classifier.options.lambda = 0.02; % regularization parameter for the 3rd-way (i.e., trial-way), which can be more precisely decided by cross-validation
-% approach.classifier.options.lambda = [0, 0.2];
-% approach.classifier.options.mode = 'sync';
+approach.classifier.learner = 'L1MCCA';
+approach.classifier.options.harmonics = 2;
+approach.classifier.options.max_iter = 200; % the maximal number of iteration for running L1MCCA
+approach.classifier.options.n_comp = 1;  % number of projection components for learning the reference signals
+approach.classifier.options.lambda = 0.02; % regularization parameter for the 3rd-way (i.e., trial-way), which can be more precisely decided by cross-validation
+approach.classifier.options.lambda = [0, 0.2];
+approach.classifier.options.mode = 'sync';
 %% Mset CCA
 % approach.classifier.learner = 'MSETCCA';
 % approach.classifier.options.n_comp = 1;
@@ -128,9 +128,9 @@ report = 0;
 % approach.classifier.options.parameters.nor.loc = 'true'; 
 % approach.classifier.options.parameters.see = 7332; % seed
 %% TRCA
-approach.classifier.learner = 'TRCA';
-approach.classifier.options.num_fbs = 5;
-approach.classifier.options.is_ensemble = 0;
+% approach.classifier.learner = 'TRCA';
+% approach.classifier.options.num_fbs = 5;
+% approach.classifier.options.is_ensemble = 0;
 %% FBCCA
 % approach.classifier.learner = 'FBCCA';
 % approach.classifier.options.harmonics = 5;
